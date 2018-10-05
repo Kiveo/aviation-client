6a35b1782415115994be2093ac8c795c7ac6fa0f
unknown <mavericktesini@gmail.com>
2018-10-04 20:09:22 -0400

    stub a form container for airplanes

--- src/containers/Airplanes.js
+++ src/containers/Airplanes.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Airplanes.css';
 import AirplaneCard from '../components/AirplaneCard';
+import AirplaneForm from './AirplaneForm';
 
-const Airplanes = (props) => (
-  <div className="AirplanesContainer">
-    <h1>Airplanes</h1>
-    <div className="AirplanesFlexBox">
-    {props.airplanes.map(airplane => <AirplaneCard airplane={airplane} key={airplane.id}/> )}
-    </div>
-  </div>
-);
+class Airplanes extends Component {
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     name: ''
+  //   }
+  // }
+
+  render() {
+    return(
+      <div className="AirplanesContainer">
+        <h1>Airplanes</h1>
+        <div className="AirplanesFlexBox">
+        {this.props.airplanes.map(airplane => <AirplaneCard airplane={airplane} key={airplane.id}/> )}
+        </div>
+        <AirplaneForm />
+      </div>
+    )
+  }
+}
 
 export default Airplanes;

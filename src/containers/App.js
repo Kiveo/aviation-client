397b12944474c0a5ade1f96c549d6832028b6104
unknown <mavericktesini@gmail.com>
2018-10-05 03:01:09 -0400

    remove component state from app and add redux state provider

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,19 +5,13 @@ import Airplanes from './Airplanes';
 const API_URL = process.env.REACT_APP_API_URL
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      airplanes: [],
-      loading: true
-    }
 
   }
   // once mounted, fetch from API via lifecycle method: AFTER initial render
   componentDidMount() {
     fetch(`${API_URL}/airplanes`)
       .then(response => response.json() )
-      .then(airplanes => this.setState({ airplanes, loading: false }) )
+      // .then(airplanes => this.setState({ airplanes, loading: false }) )
     }
     
   render() {
@@ -26,11 +20,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to Aviation Client</h1>
         </header>
-        {this.state.loading ? 
-          <h1>Loading...</h1> :
-
-        <Airplanes airplanes={this.state.airplanes} />
-        }
+        {this.state.loading ? <h1>...Loading...</h1> : <Airplanes /> }
       </div>
     );
   }

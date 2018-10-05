c966252895aaf2687528c7d80b9369aa9c65030f
unknown <mavericktesini@gmail.com>
2018-10-05 04:42:03 -0400

    stub out form fields

--- src/containers/AirplaneForm.js
+++ src/containers/AirplaneForm.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import AirplaneFormData from '../reducers/airplaneFormData';
 
 class AirplaneForm extends Component {
   render() {
+    const {name, seats, img_url, lesson_rate, identifier} = this.props.airplaneFormData;
+
     return(
-      <form>
-        Add your favorite aircraft!
-        <input type="text" />
+      <form className="AirplaneForm">
+        <p>Add your favorite aircraft!</p>
+        <label>Make/Model</label>
+        <input type="text" name="name" value={name} /> 
+        <label>Seats</label>
+        <input type="text" name="seats" value={seats} />
+        <label>Image URL(http://www...)</label>
+        <input type="text" name="img_url" value={img_url} />
+        <label>Lesson Price Per Hour</label>
+        <input type="text" name="lesson_rate" value={lesson_rate} />
+        <label>FAA identifier(N-Number)</label>
+        <input type="text" name="identifier" value={identifier} />
+
         <input type="submit" />
       </form>
     );
   }
 }
 
-export default AirplaneForm;
+const mapStateToProps = (state) => {
+  return {
+    airplaneFormData: state.airplaneFormData
+  }
+}
+
+export default connect(mapStateToProps)(AirplaneForm);

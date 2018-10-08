ad639a9e84a877c994791f99d3aadd59db098de1
unknown <mavericktesini@gmail.com>
2018-10-08 15:24:12 -0400

    stub out navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import Airplanes from '../containers/Airplanes';
+// import Airplanes from '../containers/Airplanes';
+import NavBar from '../components/NavBar';
+
 import './App.css';
 
 const App = () => {
     return (
       <div className="App">
-        <header className="App-header">
+        <NavBar />
+        {/* <header className="App-header">
           <h1 className="App-title">Welcome to Aviation Client</h1>
         </header>
-        <Airplanes />
+        <Airplanes /> */}
+        
       </div>
     );
   }

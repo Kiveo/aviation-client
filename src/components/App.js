8f07653dd1e66db74c2e4102afc8bc6ad42136f1
unknown <mavericktesini@gmail.com>
2018-10-08 14:38:21 -0400

    convert app to stateless component

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-
-import Airplanes from './Airplanes';
+import React from 'react';
+import Airplanes from '../containers/Airplanes';
 import './App.css';
 
-class App extends Component {
-
-  render() {
+const App = () => {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to Aviation Client</h1>
         </header>
-        {/* {this.state.loading ? <h1>...Loading...</h1> : <Airplanes /> } */}
         <Airplanes />
       </div>
     );
   }
-}
 
 export default App;

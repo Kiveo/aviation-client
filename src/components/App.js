be023107691dcca99f0603b23d472afbaec7f812
unknown <mavericktesini@gmail.com>
2018-10-09 00:16:40 -0400

    implement react router basic functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,38 @@
 import React from 'react';
-// import Airplanes from '../containers/Airplanes';
-import NavBar from '../components/NavBar';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
+import Airplanes from '../containers/Airplanes';
+import {BottomBanner} from './BottomBanner';
 import './App.css';
 
-const App = () => {
+const Header = () => {
+  return (
+    <header className="App-header">
+      <h1 className="App-title">Welcome to Aviation Client</h1>
+    </header>
+  )
+}
+
+const Home = () => {
     return (
       <div className="App">
-        <NavBar />
-        {/* <header className="App-header">
-          <h1 className="App-title">Welcome to Aviation Client</h1>
-        </header>
-        <Airplanes /> */}
-        
+        <Header />
+        <Airplanes />
+        <BottomBanner />
       </div>
     );
   }
+
+const App = () => {
+  return (
+    <Router>
+      <Route exact path="/" component={Home}/>
+    </Router>
+  )
+}
 
 export default App;
---
+++ src/components/BottomBanner.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const BottomBanner = () => (
+  <footer className="App-footer">
+    <p>&copy; 2018 Maverick Tesini </p>
+  </footer>
+);

77d091ec540e4c862d8fd1e956eeefb7ea8adc7c
unknown <mavericktesini@gmail.com>
2018-10-10 00:42:36 -0400

    implement navlink styles

--- src/components/HeadNav.js
+++ src/components/HeadNav.js
@@ -4,11 +4,15 @@ import { NavLink } from 'react-router-dom';
 export const HeadNav = () => {
   return (
     <header className="App-header">
-      <h1 className="App-title">Welcome to Aviation Client</h1>
+      <h1 className="App-title">Aviation Client</h1>
       <div className="NavBar">
         <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/new">Add To Listings</NavLink>
+        <NavLink to="/about"
+          activeStyle={{borderRadius: 10, background: 'rgba(250,250,250,0.2)'}}
+        >About</NavLink>
+        <NavLink to="/new" 
+          activeStyle={{borderRadius: 10, background: 'rgba(250,250,250,0.2)'}}
+        >Add To Listings</NavLink>
       </div>
     </header>
   );

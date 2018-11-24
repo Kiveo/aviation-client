16fd72dd14b1d7cbaaddc3e83c252c209bffbe0e
unknown <mavericktesini@gmail.com>
2018-11-23 22:22:15 -0500

    add edit to index cards

--- src/components/AirplaneCard.js
+++ src/components/AirplaneCard.js
@@ -3,13 +3,21 @@ import {Link} from 'react-router-dom';
 
 const AirplaneCard = (props) => (
   <div className="AirplaneCard">
-    <Link to={"/airplanes/" + props.airplane.id} className="AirplaneLink"><h3>{props.airplane.name}</h3></Link>
+    {props.showButtons === "true" ?
+      <Link to={"/airplanes/" + props.airplane.id} className="AirplaneLink"><h3>{props.airplane.name}</h3></Link>
+      : <h1>{props.airplane.name}</h1> 
+    }
     <p>Seats: {props.airplane.seats}</p>
     <img className="AirplaneImg" src={props.airplane.img_url} alt={'Image of ' + props.airplane.name}/>
     <p>FAA Identifier: {props.airplane.identifier}</p>
     <p>Hourly Rate: ${props.airplane.lesson_rate}</p>
-    <button className="AirplaneDelete" onClick={() => props.delete(props.airplane)}>Remove</button>
-  </div>
+    {props.showButtons === "true" ?
+      <Link to={"/airplanes/" + props.airplane.id + "/edit"} className="AirplaneEditLink"><h3>Edit</h3></Link> : null
+    }
+    {props.showButtons === "true" ?
+      <button className="AirplaneDelete" onClick={() => props.delete(props.airplane)}>Remove</button>
+    :null }
+    </div>
 );
 
 export default AirplaneCard;

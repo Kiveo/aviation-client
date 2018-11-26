71b57c7092a4ad871e07e1dacdc3235f313d7388
Mav <mavericktesini@gmail.com>
2018-11-26 18:30:12 -0500

    redo basic navbar css

--- src/components/HeadNav.js
+++ src/components/HeadNav.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 export const HeadNav = () => {
   return (
     <header>
-      <div className="NavBar">
+      <div className="navBar">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/airplanes"
           activeStyle={{borderRadius: 10, background: 'rgba(250,250,250,0.2)'}}        

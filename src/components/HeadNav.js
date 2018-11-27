292f66ef74fb2f9714b71954e46fd77e6611f376
Mav <mavericktesini@gmail.com>
2018-11-26 20:12:47 -0500

    create basic js responsive nav function

--- src/components/HeadNav.js
+++ src/components/HeadNav.js
@@ -3,13 +3,21 @@ import { NavLink } from 'react-router-dom';
 
 export const HeadNav = () => {
   let handleResponsive = () => {
-    console.log("clicked the link");
+    console.log("clicked the button");
+    let icon = document.getElementById("navIcon");
+    if (icon.className === "navIcon") {
+      icon.className += " responsive";
+    } else {
+      icon.className = "navIcon";
+    }
   }
+
   return (
     <header>
       <div className="navBar">
         {/* hamburger unicode: &#9776; */}
-        <button className="navIcon" onClick={handleResponsive}>&#9776;</button>
+        <button className="navIcon" id="navIcon" onClick={handleResponsive}>&#9776;</button>
+        {/* airplane unicode: &#9992; */}
         <NavLink to="/" className="navLogo">&#9992;</NavLink>
         <NavLink to="/airplanes"
           activeStyle={{borderRadius: 10, background: 'rgba(250,250,250,0.2)'}}        
